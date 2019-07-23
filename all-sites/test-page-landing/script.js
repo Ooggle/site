0a590459c39b2445f7f2b0bd9e64d4e96fcca40a
Ooggle <33269056+Ooggle@users.noreply.github.com>
2019-07-23 14:27:07 +0200

    add test page opening rev.0.8
    a quick lib'less bunch of opening pages

---
+++ all-sites/test-page-landing/script.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    setTimeout(() => {
+        document.getElementById("presentation-body").style.display = "none";
+        document.getElementById("site-body").style.opacity = "1";
+    }, 2000);
+}
