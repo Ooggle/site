e558cc283cb8661cd364ab4588e7ba449fde13e2
Ooggle <33269056+Ooggle@users.noreply.github.com>
2019-08-05 17:38:56 +0200

    enhanced version of landing page

--- all-sites/test-page-landing/script.js
+++ all-sites/test-page-landing/script.js
@@ -2,5 +2,7 @@ window.onload = function () {
     setTimeout(() => {
         document.getElementById("presentation-body").style.display = "none";
         document.getElementById("site-body").style.opacity = "1";
+        document.getElementById("site-body").style.marginTop = "50px";
+        document.body.style.overflow = "scroll";
     }, 2000);
 }
