7bf40b6d0375e869bb73110b34dcc7cc4b9ce38f
Ooggle <33269056+Ooggle@users.noreply.github.com>
2019-08-06 11:16:14 +0200

    landing page fix
    fix scrollbar indicator, add comment and ajust some things into landing page

--- all-sites/test-page-landing/script.js
+++ all-sites/test-page-landing/script.js
@@ -4,6 +4,16 @@ window.onload = function () {
         document.getElementById("site-body").style.opacity = "1";
         document.getElementById("site-body").style.marginTop = "50px";
         document.body.style.overflow = "scroll";
+
+        var h = document.documentElement, 
+        b = document.body,
+        st = 'scrollTop',
+        sh = 'scrollHeight';
+
+        var percent = (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
+
+        document.getElementById("scrollbar-indicator-elem").style.height = percent + "%";
+        document.getElementById("scrollbar-indicator-elem").style.opacity = 1;
     }, 2000);
 }
 
